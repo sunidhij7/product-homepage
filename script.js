a7010e473a1cdd605df510961e2fd2d78bc4e056
Sunidhi Jain <sunidhi.jain@brevo.com>
2024-08-05 12:55:15 +0530

    added dropdown functionality

--- script.js
+++ script.js
@@ -45,3 +45,46 @@ const accordion = document.querySelectorAll(".accordian--trigger");
       accordion[i].nextElementSibling.style.display = "none";
   }
   });
+
+
+//   dropdown
+const dropdownOpenTrigger = document.querySelector(
+    ".dropdown__trigger-button"
+  );
+  const dropdown = document.querySelector(".dropdown__panel");
+  const options = document.querySelectorAll(".dropdown__panel__option");
+  let lastSelectedOption = null;
+
+
+  dropdownOpenTrigger.addEventListener("click", function () {
+    dropdown.classList.toggle("show");
+  });
+
+  // replacing drop button text
+  options.forEach((option) => {
+    option.addEventListener("click", function (event) {
+
+      //removing previous option highlight
+      if (lastSelectedOption) {
+        lastSelectedOption.classList.remove('colored');
+    }
+
+      const selectedText = event.target.innerText;
+      dropdownOpenTrigger.innerText = selectedText;
+      event.target.classList.add("colored")
+
+      //changing last option
+      lastSelectedOption = event.target;
+
+      dropdown.classList.remove("show");
+
+    });
+  });
+
+  window.onclick = function (event) {
+    if (!event.target.matches(".dropdown__trigger-button")) {
+      if (dropdown.classList.contains("show")) {
+        dropdown.classList.remove("show");
+      }
+    }
+  };
